74061f016dc9bc6a08f7dbe5eb332a1b9688e645
Maysa-Bilac <maysa.bilac@hotmail.com>
2022-04-27 13:52:39 -0300

    Ajustes

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,13 +21,13 @@ function calculaAreaSala() {
         document.getElementById("janela").value == "" ||
         document.getElementById("porta").value == "")
         {
-            alert('Por favor, preencha todos os campos');
+            swal('Por favor, preencha todos os campos!');
         }else 
         if(areaParede > 15 || areaParede < 1){
-            alert("A aréa total da parede deve ser entre 1m e 15m");
+            swal("A aréa total da parede deve ser entre 1m e 15m! ");
         }else
         if(alturaParede_HTML < (alturaPorta + 0.3)){
-            alert("A altura da parade tem que ser 30 centímetros maior que a altura da porta");
+            swal("A altura da parede deve ser no mínimo 30 centímetros maior que a altura da porta!");
         }
         else{
                 if(janela_HTML > 0 || porta_HTML > 0 ){
@@ -47,9 +47,9 @@ function calculaAreaSala() {
 }   
 
 function resultado (msg, litroTinta, areaSala, somaPortaJanela){
-    alert("Seu ambiente possui: " +areaSala+ "m² \n"+ 
-            "Será necessário: " +litroTinta+ "L de tinta\n"+
-            "Soma de porta e janela equivale a " +somaPortaJanela+ "m²\n"+
+    swal("Seu ambiente possui: " +areaSala+ "m² \n \n"+ 
+            "Será necessário: " +litroTinta+ "L de tinta \n \n"+
+            "Soma de porta e janela equivale a: " +somaPortaJanela+ "m² \n \n"+
             msg );
 }
 
@@ -59,7 +59,7 @@ function latasNecessarias (litroTinta){
     let lata25L = 0;
     let lata05L = 0;
 
-    while (litroTinta > 0.5){
+    while (litroTinta >= 0.5 && litroTinta != 0){
         while (litroTinta >= 18 && litroTinta != 0) {
             lata18L = lata18L + 1;
             litroTinta = litroTinta - 18;
@@ -82,7 +82,8 @@ function latasNecessarias (litroTinta){
         litroTinta = litroTinta - 0.5;
         console.log(litroTinta);
     }
-    return "Serão necessárias: "+ lata18L +" lata(s) de 18L | "+ lata36L +" lata(s) de 3,6L | "+ lata25L +" lata(s) de 2,5L | "+ lata05L +" lata(s) de 0,5L ";
+    return "Serão necessárias: " + "\n" + lata18L +" lata(s) de 18L \n"+ lata36L +" lata(s) de 3,6L \n"+ lata25L +" lata(s) de 2,5L \n"+ lata05L +" lata(s) de 0,5L ";
 }
 
 document.getElementById("calcular").addEventListener("click", calculaAreaSala);
+
